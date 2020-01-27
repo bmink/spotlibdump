f47c0a46dde182161716da1b13043b53d1ae7483
Barna Mink <barnamink@gmail.com>
2020-01-26 21:41:27 -0800

    .

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -v
+
+./spotlibdump "1210243824" "BQAuTPbRC9txOUkSw1vFBGFFNiZuUDSEoTsvotsH1VBl03Lwg6rXjNBdzZlWW5ckuQFg1_berhaSxdO--mYnLv4HhHkrvjfrpvHHT98lxUYbsXeOafqmaTvHU32zDcrVh4zdc8_ZPr4Zzw7-EzRUHwrHqQWV7h2Ic4ZwHyM"
+
