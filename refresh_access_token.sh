82f1e4837442f3688e7715382eac79a637725be8
Barna Mink <barnamink@gmail.com>
2020-07-27 15:25:49 -0700

    .

--- refresh_access_token.sh
+++ refresh_access_token.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Based on:
-# https://gist.github.com/hughrawlinson/1d24595d3648d53440552436dc215d90#
-#
-# Usage: authorize.sh <client_id> <client_secret>
-#
-# Generates an access and a refresh token. The access token will be good for
-# an hour only, so we don't really care about it. The important thing is the
-# refresh token. It should be put into a file named .refreshtoken. The program
-# will read it and acquire a new access token before doing anything else.
-#
-#
-# On your Spotify dev dashboard, your app must have "http://localhost:8082/'
-# as its redirect_uri.
-
 if [[ -z "$REDIS_ADDR" ]]; then
 	echo "REDIS_ADDR is not set."
 	exit -1
